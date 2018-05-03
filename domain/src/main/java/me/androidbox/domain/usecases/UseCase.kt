f8c5ae5da26f545d32da1d200fd2da6f744d7e47
= <stevekitkat@gmail.com>
2018-05-03 22:54:25 +0700

    Added use case

--- domain/src/main/java/me/androidbox/domain/usecases/UseCase.kt
+++ domain/src/main/java/me/androidbox/domain/usecases/UseCase.kt
@@ -1,5 +1,13 @@
 package me.androidbox.domain.usecases
 
-abstract class UseCase {
+import io.reactivex.Observable
+import me.androidbox.domain.core.Transformer
 
+abstract class UseCase<T>(private val transformer: Transformer<T>) {
+
+    abstract fun createObservable(data: Map<String, Any>? = null): Observable<T>
+
+    fun observable(withData: Map<String, Any>? = null): Observable<T> {
+        return createObservable(withData).compose(transformer)
+    }
 }
